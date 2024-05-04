8995e7fc22a9d353e53ee6939704eeb3d868b901
tanyjnaaman <tannaaman@hotmail.com>
2024-05-04 05:54:28 +0000

    Update script

--- score_with_correction.sh
+++ score_with_correction.sh
@@ -49,8 +49,10 @@ do
     
     # only generated text for probability
     # python -m src.experiments.language_model_experiments.score_sample_probability --csv-file-path $file_path --batch-size 16 --no-include-prompt
+    python -m src.experiments.language_model_experiments.score_sample_probability --csv-file-path $file_path --batch-size 16 --no-include-prompt --condition_on_prompt
+
 
     # with human assistant format for probability under generation model (for correction)
-    python -m src.experiments.language_model_experiments.score_sample_probability_correction --csv-file-path $file_path --batch-size 16 --add-human-assistant-format --sampling-type $sampling_type --language-model "ethz-spylab/rlhf-7b-harmless" --sampling-temperature $temperature
+    python -m src.experiments.language_model_experiments.score_sample_probability_correction --csv-file-path $file_path --batch-size 16 --add-human-assistant-format --sampling-type $sampling_type --language-model "ethz-spylab/rlhf-7b-harmless" --sampling-temperature $temperature --condition_on_prompt
 
 done
