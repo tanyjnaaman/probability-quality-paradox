be8af0b444c5b3ac6eea3df876457ca799e8ed82
tanyjnaaman <tannaaman@hotmail.com>
2024-05-26 03:42:05 +0000

    Cleanup

--- generate.sh
+++ generate.sh
@@ -8,24 +8,26 @@ prompt_selection_seed=42
 num_prompts=1000
 num_generations_per_prompt=2
 batch_size=2
-device="auto"
+device="cuda:1"
 sampling_types=(
     # "ancestral_strict"
     # "ancestral"
     # "top_k30" 
-    # "eta_n00009"
-    # "top_p090"
-    # "top_p095" 
-    # "typical_p090"
-    "greedy"
+    "eta_n00009"
+    "top_p090"
+    "top_p095" 
+    "typical_p090"
+    # "greedy"
 )
 sampling_temperatures=(
     # 0.5
     # 0.75
+    0.95
+    # 1.0 
+    1.05
     # 1.25
-    1.0 
-    1.5 
-    2.0
+    # 1.5 
+    # 2.0
 )
 prompt_start_idx=0
 prompt_dataset_split="test"
@@ -35,7 +37,5 @@ do
 for sampling_temperature in "${sampling_temperatures[@]}"
 do
     python -m src.experiments.language_model_experiments.sample_from_model --model $model --max-length $max_length --prompt-selection-seed $prompt_selection_seed --num-prompts $num_prompts --num-generations-per-prompt $num_generations_per_prompt --batch-size $batch_size --device $device --sampling-type $sampling_type --sampling-temperature $sampling_temperature --human-assistant-format --prompt-start-idx $prompt_start_idx --prompt-dataset-split $prompt_dataset_split
-    # python -m src.experiments.language_model_experiments.sample_from_model --model $model --max-length $max_length --prompt-selection-seed $prompt_selection_seed --num-prompts $num_prompts --num-generations-per-prompt $num_generations_per_prompt --batch-size $batch_size --device $device --sampling-type $sampling_type --sampling-temperature $sampling_temperature --prompt-start-idx $prompt_start_idx --prompt-dataset-split $prompt_dataset_split
-
 done
 done
--- score_with_correction.sh
+++ score_with_correction.sh
@@ -1,166 +1,76 @@
 #!/bin/bash
 set -euxo pipefail
 
-file_paths=(
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t0.75_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t2.0_humanassistant.csv"
-
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t0.75_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.25_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t2.0_humanassistant.csv"
-
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t0.75_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.25_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t2.0_humanassistant.csv"
-
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t0.75_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.25_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t2.0_humanassistant.csv"
-
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t0.75_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.25_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t2.0_humanassistant.csv"
-
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t0.75_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.25_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t2.0_humanassistant.csv"
-
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t0.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t0.74_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.0_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.25_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.5_humanassistant.csv"
-    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t2.0_humanassistant.csv"
-)
-sampling_types=(
-    "ancestral_strict"
-    "ancestral_strict"
-    "ancestral_strict"
-    "ancestral_strict"
-    "ancestral_strict"
-    "ancestral_strict"  
-
-    "ancestral"
-    "ancestral"
-    "ancestral"
-    "ancestral"
-    "ancestral"
-    "ancestral"
-
-    "eta_n00009"
-    "eta_n00009"
-    "eta_n00009"
-    "eta_n00009"
-    "eta_n00009"
-    "eta_n00009"
-
-    "top_k30"
-    "top_k30"
-    "top_k30"
-    "top_k30"
-    "top_k30"
-    "top_k30"
-
-    "top_p090"
-    "top_p090"
-    "top_p090"
-    "top_p090"
-    "top_p090"
-    "top_p090"
-
-    "top_p095"
-    "top_p095"
-    "top_p095"
-    "top_p095"
-    "top_p095"
-    "top_p095"
-
-    "typical_p090"
-    "typical_p090"
-    "typical_p090"
-    "typical_p090"
-    "typical_p090"
-    "typical_p090"
-)
-temperatures=(
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
-
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
-
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
-
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
-
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
-
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
-
-    "0.5"
-    "0.75"
-    "1.0"
-    "1.25"
-    "1.5"
-    "2.0"
+file_paths_sampling_types_temps=(
+    # ancestral_strict
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t0.5_humanassistant.csv,ancestral_strict,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t0.75_humanassistant.csv,ancestral_strict,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t0.75_humanassistant.csv,ancestral_strict,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.0_humanassistant.csv,ancestral_strict,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.0_humanassistant.csv,ancestral_strict,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.25_humanassistant.csv,ancestral_strict,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t1.5_humanassistant.csv,ancestral_strict,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_strict_t2.0_humanassistant.csv,ancestral_strict,2.0"
+    # ancestral
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t0.5_humanassistant.csv,ancestral,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t0.75_humanassistant.csv,ancestral,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t0.75_humanassistant.csv,ancestral,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.0_humanassistant.csv,ancestral,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.0_humanassistant.csv,ancestral,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.25_humanassistant.csv,ancestral,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t1.5_humanassistant.csv,ancestral,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_ancestral_t2.0_humanassistant.csv,ancestral,2.0"
+    # eta_n00009
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t0.5_humanassistant.csv,eta_n00009,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t0.75_humanassistant.csv,eta_n00009,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t0.75_humanassistant.csv,eta_n00009,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.0_humanassistant.csv,eta_n00009,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.0_humanassistant.csv,eta_n00009,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.25_humanassistant.csv,eta_n00009,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t1.5_humanassistant.csv,eta_n00009,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_eta_n00009_t2.0_humanassistant.csv,eta_n00009,2.0"
+    # top_k30
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t0.5_humanassistant.csv,top_k30,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t0.75_humanassistant.csv,top_k30,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t0.75_humanassistant.csv,top_k30,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.0_humanassistant.csv,top_k30,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.0_humanassistant.csv,top_k30,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.25_humanassistant.csv,top_k30,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t1.5_humanassistant.csv,top_k30,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_k30_t2.0_humanassistant.csv,top_k30,2.0"
+    # top_p090
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t0.5_humanassistant.csv,top_p090,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t0.75_humanassistant.csv,top_p090,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t0.75_humanassistant.csv,top_p090,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.0_humanassistant.csv,top_p090,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.0_humanassistant.csv,top_p090,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.25_humanassistant.csv,top_p090,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t1.5_humanassistant.csv,top_p090,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p090_t2.0_humanassistant.csv,top_p090,2.0"
+    # top_p095
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t0.5_humanassistant.csv,top_p095,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t0.75_humanassistant.csv,top_p095,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t0.75_humanassistant.csv,top_p095,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.0_humanassistant.csv,top_p095,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.0_humanassistant.csv,top_p095,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.25_humanassistant.csv,top_p095,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t1.5_humanassistant.csv,top_p095,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_top_p095_t2.0_humanassistant.csv,top_p095,2.0"
+    # typical_p090
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t0.5_humanassistant.csv,typical_p090,0.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t0.75_humanassistant.csv,typical_p090,0.75"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t0.75_humanassistant.csv,typical_p090,0.95"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.0_humanassistant.csv,typical_p090,1.0"
+    "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.0_humanassistant.csv,typical_p090,1.05"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.25_humanassistant.csv,typical_p090,1.25"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t1.5_humanassistant.csv,typical_p090,1.5"
+    # "ethz-spylab-rlhf-7b-harmless_l256_promptseed42_numprompt1000_numgenerations2_typical_p090_t2.0_humanassistant.csv,typical_p090,2.0"
 )
 batch_size=2
 
-for i in "${!file_paths[@]}"
-do
-
-    file_path=${file_paths[$i]}
-    sampling_type=${sampling_types[$i]}
-    temperature=${temperatures[$i]}
-
-    echo "Scoring $file_path"
+for line in $file_paths_sampling_types_temps; do
+    IFS=',' read file_path sampling_type temperature <<< $line
+    echo "Scoring $file_path with $sampling_type and temperature $temperature"
     # with human assistant format for reward model
     python -m src.experiments.language_model_experiments.score_sample_reward --csv-file-path $file_path --batch-size $batch_size --add-human-assistant-format
     
@@ -171,5 +81,4 @@ do
 
     # with human assistant format for probability under generation model (for correction)
     python -m src.experiments.language_model_experiments.score_sample_probability_correction --csv-file-path $file_path --batch-size $batch_size --add-human-assistant-format --sampling-type $sampling_type --language-model "ethz-spylab/rlhf-7b-harmless" --sampling-temperature $temperature --condition-on-prompt
-
 done
